92d8bec89d635b83550b77810e6ef1dec3421bf5
OTimmy <timmy.olof.olsson@gmail.com>
2021-02-09 18:34:58 +0000

    Deploying to master from  @ f40138387f71a206a7dfd690fa4281d8ae4fc668 🚀

---
+++ static/m6xxsIuAhYqV5cQIdWaQw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-9dba724dc20bfd3c3476.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8f7647a2f8f74d3ae521.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
