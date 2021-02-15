44459a1baca1292c4f55a7ca3b8a5dde3ca3c147
timmy <you@example.com>
2021-02-15 21:47:34 +0100

    Added font

--- nextjs-blog/pages/sidebar.js
+++ nextjs-blog/pages/sidebar.js
@@ -4,10 +4,15 @@ import styles from './sidebar.module.css'
 function SideBar() {
     return    (
         <nav className={styles.sidebar}>
-            <div className={`container-sm shadow-sm p-3 mb-5 bg-body  bg-primary text-white ${styles.fill}`}>
+            <div className={`container-sm shadow-sm p-3 mb-0 bg-body  bg-primary text-white ${styles.fill}`}>
                 <div className="row">
-                    <div className="col">
+                    <div className={`col ${styles.title}`}>
                         Skafferiet
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col bg-white h-75">
+                        asdasd
                     </div>
                 </div>
             </div>
